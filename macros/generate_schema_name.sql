e3e1493313b8d137aa572da5ba2adf690449bb92
Sanny Prawira <sanny.prawira@fishtownanalytics.com>
2024-10-24 10:08:54 +0000

    com 2

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,3 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- if (target.name == 'prod' or target.name == 'ci') and custom_schema_name is not none -%}
-        {{ (custom_schema_name | trim) }}
-    {%- else -%}
-        {{ (target.schema) }}
-    {%- endif -%}
+    {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
